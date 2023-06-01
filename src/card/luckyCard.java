56b18def02b9e37fd032990ccc19a718ae1f82e8
wotalora <133712267+wotalora@users.noreply.github.com>
2023-05-31 23:15:19 -0400

    Update and rename CardTrick.java to luckyCard.java
    Code creating the LuckyCard

--- src/card/luckyCard.java
+++ src/card/luckyCard.java
@@ -14,7 +14,17 @@ public class CardTrick {
     
     public static void main(String[] args)
     {
-        Card[] magicHand = new Card[7];
+        
+        public void luckyCard(){
+        
+        // ** Creates an aleatory luckyCard     
+            Card c1 = new Card();
+            c1.setValue((1));   // @author William Otalora SN 991691907
+            c1.setSuit(Card.SUITS[(3)];  // @author William Otalora SN 991691907
+         
+        }
+            
+     
         
         for (int i=0; i<magicHand.length; i++)
         {
@@ -22,7 +32,23 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        // @author ** William Otalora SN 991691907
         
+            public static void main(String[] args)
+    {
+        Card[] magicHand = new Card[7];
+        
+        for (int i=0; i<magicHand.length; i++)
+        {
+            Card c = new Card();
+            c.setValue((int) (Math.random()*13 + 1));   // @author William Otalora SN 991691907
+            //c.setValue(insert call to random number generator here)
+            c.setSuit(Card.SUITS[(int) (Math.random()*3)]);  // @author William Otalora SN 991691907
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        }
+        
+        //Scanner input= new Scanner(System.in);
+        System.out.print("Magic Hand"+ Arrays.toString(magicHand));
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
